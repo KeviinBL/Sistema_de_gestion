b41f192e5ccfaa8bcd36b934764cc5a87ad50d95
Kevin E Bejarano Lancheros <141942296+KeviinBL@users.noreply.github.com>
2023-10-28 18:31:11 -0500

    Add files via upload

---
+++ Sistemas_de_gestion/clientes/clientes/src/test/java/com/gestion/clientes/ClientesApplicationTests.java
@@ -0,0 +1,18 @@
+package com.gestion.clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
